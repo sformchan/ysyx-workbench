ece5fdebab97536bd0da7ed6bd67c394673c68fb
tracer-ysyx <tracer@ysyx.org>
2025-01-18 10:55:55 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:55:55 up  1:48,  1 user,  load average: 0.07, 0.03, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,6 +55,22 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args)
+{
+  int step;
+  int result = sscanf(args, "%d", &step);
+  if(result == 1)
+  {
+    cpu_exec(step);    
+  }
+  else
+  {
+    cpu_exec(1);
+  }
+  return 0;
+}
+
+
 static struct {
   const char *name;
   const char *description;
@@ -63,6 +79,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Let the program step", cmd_si}
 
   /* TODO: Add more commands */
 
