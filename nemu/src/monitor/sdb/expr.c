e5d00ce45b0c64843508327d71645c002347049a
tracer-ysyx <tracer@ysyx.org>
2025-01-19 15:07:41 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:07:41 up  5:52,  1 user,  load average: 0.02, 0.03, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -210,7 +210,7 @@ uint32_t eval(int p, int q) {
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
-    return eval(q + 1, q - 1);
+    return eval(p + 1, q - 1);
   }
   else {
     int op = -1;
