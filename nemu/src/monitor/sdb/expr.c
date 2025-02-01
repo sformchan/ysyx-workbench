2cccfa04f6d6430ea3965a34e6b54d28a12649d3
tracer-ysyx <tracer@ysyx.org>
2025-02-01 15:44:08 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:08 up  1:26,  1 user,  load average: 0.15, 0.09, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -134,18 +134,18 @@ static bool make_token(char *e) {
             nr_token++;
             break;
           case '*':
-            if(i == 0 || tokens[i - 1].type == '(' || 
+           /* if(i == 0 || tokens[i - 1].type == '(' || 
                          tokens[i - 1].type != 2   || 
                          tokens[i - 1].type != 16)
             {
               tokens[nr_token].type = 7;
               nr_token++;
-            }
-            else
-            {
-              tokens[nr_token].type = '*';
-              nr_token++;
-            }    
+            } 
+            else */
+            
+            tokens[nr_token].type = '*';
+            nr_token++;
+                
             break; 
           case '/':
             tokens[nr_token].type = '/';
