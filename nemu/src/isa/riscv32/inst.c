ed760e472d1347690cbe132f65f2ead936252290
tracer-ysyx <tracer@ysyx.org>
2025-03-04 22:46:45 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:46:45 up  5:17,  1 user,  load average: 0.87, 0.52, 0.37

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,7 +24,7 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_N, TYPE_J // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
@@ -32,6 +32,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immJ() do { *imm = (BITS(i, 31, 31) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) |(BITS(i, 30, 21) << 1); } while(0);
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
@@ -42,7 +43,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
-    case TYPE_N: break;
+    case TYPE_J:                   immJ(); break;
+    case TYPE_N:                           break;
     default: panic("unsupported type = %d", type);
   }
 }
@@ -59,7 +61,7 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
-  //INSTPAT("??????? ????? ????? 000 ????? 00100 11", li     , I, R(rd) = imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, s->dnpc = SEXT(imm * 2, 20) + s->pc, R(rd) = s->snpc);
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = imm + src1);
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
