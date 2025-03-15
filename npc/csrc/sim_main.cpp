613484f51b5ff19f1f0a054d5bbe0b195f4b2c7d
tracer-ysyx <tracer@ysyx.org>
2025-03-16 02:22:59 +0800

    > sim RTL ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:22:59 up  4:19,  1 user,  load average: 0.28, 0.20, 0.09

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv)
 		//tfp->dump(contextp->time());
 		contextp->timeInc(1);
 		printf("pc:   %x\n", top->pc);
-		printf("inst: %x\n", top->inst);
+		printf("inst: %u\n", top->inst);
 		printf("gpr0: %x\n", top->gpr0);
 		printf("gpr1: %x\n", top->gpr1);
 		printf("gpr2: %x\n", top->gpr2);
