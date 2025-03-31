383df0b893257fe559132480cfd1d21e694e9231
tracer-ysyx <tracer@ysyx.org>
2025-04-01 02:28:43 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:28:43 up  3:24,  1 user,  load average: 0.18, 0.25, 0.20

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -16,14 +16,17 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  if(dst == NULL || src == NULL) return dst;
-  size_t i;
-  for(i = 0; src[i] != '\0'; i++)
-  {
-    dst[i] = src[i];
+  if (src == NULL || dst == NULL) {   // 没有所指，直接返回dst
+    return dst;
   }
-  dst[i] = src[i];
-  return dst;
+  // 当成指向字符数组处理，所以即使没有空字符，导致内存访问越界，或修改了其他有用的数据也不管，因为这是函数调用者所需要保证的，下面一些string函数都是这样对带非字符串数组
+  char *res = dst;
+  do {
+    *dst = *src;
+    dst++;
+    src++;
+  } while(*src != '\0');  
+  return res;
   //panic("Not implemented");
 }
 
@@ -33,25 +36,26 @@ char *strncpy(char *dst, const char *src, size_t n) {
 
 char *strcat(char *dst, const char *src) {
   if(dst == NULL || src == NULL) return dst;
-  size_t length = strlen(dst);
-  size_t i;
-  for(i = 0; src[i] != '\0'; i++)
-  {
-    dst[length + i] = src[i];
+  char *ans = dst;
+  while (*dst != '\0') {
+    ++dst;
   }
-  dst[length + i] = '\0';
-  return dst;
+  do {
+    *dst = *src;
+    dst++;
+    src++;
+  } while(*src != '\0');  // 先做一次，可以保证将结束字符也复制进去
+  return ans;
   //panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
   if(s1 == NULL || s2 == NULL) return 0;
-  while(*s1 && (*s1 == *s2))
-  {
+  while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
     s1++;
     s2++;
   }
-  return (int) *s1 - (int) *s2;
+  return *s1 == *s2 ? 0 : (unsigned char)*s1 < (unsigned char)*s2 ? -1 : 1;
   //panic("Not implemented");
 }
 
@@ -61,13 +65,13 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 
 void *memset(void *s, int c, size_t n) {
   if(s == NULL) return s;
-  unsigned char *dst = (unsigned char *)s;  
-  unsigned char value = (unsigned char)c;   
-  for (size_t i = 0; i < n; i++) {
-    dst[i] = value;  // 按字节设置内存
+  unsigned char *src = s;   // 先讲传入得指针，做无符号字符解释
+  while (n != 0) {
+    --n;
+    *src = c;
+    ++src;
   }
   return s;
-
   //panic("Not implemented");
 }
 
@@ -77,12 +81,13 @@ void *memmove(void *dst, const void *src, size_t n) {
 
 void *memcpy(void *out, const void *in, size_t n) {
   if(out == NULL || in == NULL || n == 0 || out == in) return out;
-  size_t i;
-  unsigned char *dst = (unsigned char *)out;
-  const unsigned char *src = (const unsigned char *)in;
-  
-  for (i = 0; i < n; i++) {
-    dst[i] = src[i];
+  unsigned char *dest = out;
+  const unsigned char *src = in;
+  while (n != 0) {
+    *dest = *src;
+    --n;
+    ++dest;
+    ++src;
   }
   return out;
   //panic("Not implemented");
