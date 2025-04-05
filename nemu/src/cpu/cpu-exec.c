89f7c57fe35cfdd709870f1fc8d402b3f9b003b4
tracer-ysyx <tracer@ysyx.org>
2025-04-05 13:42:16 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:42:16 up 48 min,  1 user,  load average: 0.61, 0.44, 0.30

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -133,7 +133,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-      printf("%d %d %d\n", nemu_state.state, NEMU_END, nemu_state.halt_ret);
+      //printf("%d %d %d\n", nemu_state.state, NEMU_END, nemu_state.halt_ret);
       // fall through
     case NEMU_QUIT: statistic();
   }
