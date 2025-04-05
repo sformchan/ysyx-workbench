3d4520b5638d4ceb9b177bc90b7b31852addce59
tracer-ysyx <tracer@ysyx.org>
2025-04-05 16:25:35 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:35 up  3:31,  1 user,  load average: 0.27, 0.28, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -393,32 +393,34 @@ uint32_t eval(int p, int q) {
         }
          
       }
-      if((sign >= 1 || sign == 0) && (tokens[i].type == '+' || tokens[i].type == '-'))
+      
+      
+      if((sign >= 1 || sign == 0) && tokens[i].type == 6) //or
       {
-        //assert(0);
         sign = 1;
         op = i;
       }
-      if((sign >= 2 || sign == 0) && (tokens[i].type == '*' || tokens[i].type == '/'))
+      if((sign >= 2 || sign == 0) && tokens[i].type == 5) //and
       {
-        printf("hello\n");
         sign = 2;
         op = i;
       }
-      if((sign >= 5 || sign == 0) && (tokens[i].type == 1 || tokens[i].type == 3))
+      if((sign >= 3 || sign == 0) && (tokens[i].type == 1 || tokens[i].type == 3)) //eq/neq
       {
         //assert(0);
-        sign = 5;
-        op = i;
-      }
-      if((sign >= 3 || sign == 0) && tokens[i].type == 5)
-      {
         sign = 3;
         op = i;
       }
-      if((sign >= 4 || sign == 0) && tokens[i].type == 6)
+      if((sign >= 4 || sign == 0) && (tokens[i].type == '+' || tokens[i].type == '-'))
       {
+        //assert(0);
         sign = 4;
+        op = i;
+      }
+      if((sign >= 5 || sign == 0) && (tokens[i].type == '*' || tokens[i].type == '/'))
+      {
+        printf("hello\n");
+        sign = 5;
         op = i;
       }
       //printf("%d %d %d\n", p, q, op);
