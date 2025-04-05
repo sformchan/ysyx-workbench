61fec707912b62ff3f89fd31f9e531889188f472
tracer-ysyx <tracer@ysyx.org>
2025-04-05 14:48:39 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:48:39 up  1:54,  1 user,  load average: 0.16, 0.28, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -409,7 +409,7 @@ uint32_t eval(int p, int q) {
     //printf("%d\n", op);
     //printf("flag is %s\n", sign ? "true" : "false");
     //printf("%c\n", tokens[op].type);
-    //printf("%d\n", op);
+    printf("%d\n", op);
     int op_type = tokens[op].type;
     //printf("%d\n", op_type);
     //op = the position of 主运算符 in the token expression;
