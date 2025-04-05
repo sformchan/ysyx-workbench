fa1f42633f85c8fc1d737291f3ed80bb276eb968
tracer-ysyx <tracer@ysyx.org>
2025-04-05 13:59:01 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:59:01 up  1:05,  1 user,  load average: 0.88, 0.54, 0.40

--- nemu/tools/gen-expr/temp.c
+++ nemu/tools/gen-expr/temp.c
@@ -1,3 +1,3 @@
 #include <stdio.h>
 #include <stdint.h>
-int main() {   uint32_t result = (27u  + 33u + ((46u)) / 38u + (((60u)) * ((43u * 68u))) /  98u * 97u + ((30u)) * 73u);   printf("%u", result);   return 0; }
+int main() {   uint32_t result = (((43u ) * (40u )));   printf("%u", result);   return 0; }
