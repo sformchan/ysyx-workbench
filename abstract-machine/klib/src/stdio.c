b74b3878fec6bc225dc63891499588e1e5853f36
tracer-ysyx <tracer@ysyx.org>
2025-04-21 23:40:20 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:40:20 up 53 min,  1 user,  load average: 0.29, 0.35, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,56 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *ptr = out;
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      switch (*fmt) {
+        case 'd': { // 整数
+          int num = va_arg(ap, int);
+          char buf[32];
+          int len = 0;
+
+          if (num < 0) {
+            *ptr++ = '-';
+            num = -num;
+          }
+          do {
+            buf[len++] = '0' + (num % 10);
+            num /= 10;
+          } while (num > 0);
+
+          while (len--) {
+            *ptr++ = buf[len];
+          }
+          break;
+        }
+        case 's': { // 字符串
+          const char *str = va_arg(ap, const char *);
+          while (*str) {
+            *ptr++ = *str++;
+          }
+          break;
+        }
+      }
+    }
+    else {
+      *ptr++ = *fmt;
+    }
+    fmt++;
+  }
+  *ptr = '\0';
+  return ptr - out;  
+  //panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int len = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return len;
+  //panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
