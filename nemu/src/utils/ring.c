d37264b38bc9f6814aeb12241f4d49d4799b87a3
tracer-ysyx <tracer@ysyx.org>
2025-04-25 13:14:02 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:14:02 up  1:03,  1 user,  load average: 0.44, 0.45, 0.43

---
+++ nemu/src/utils/ring.c
@@ -0,0 +1,31 @@
+#include <common.h>
+
+
+#define RINGBUF_SIZE 16
+
+typedef struct {
+    char buffer[128][RINGBUF_SIZE];
+    int index;
+} RingBuffer;
+
+static RingBuffer ringbuf;
+
+
+void init_ringbuf()
+{
+    ringbuf.index = 0;
+    for (int i = 0; i < RINGBUF_SIZE; i++) memset(ringbuf.buffer[i], 0, 128);
+}
+
+
+void ringbuf_push(const char *log)
+{
+    snprintf(ringbuf.buffer[ringbuf.index], 128, "%s", log);
+    ringbuf.index = (ringbuf.index + 1) % RINGBUF_SIZE;
+}
+
+void ringbuf_print()
+{
+    printf(ANSI_FG_YELLOW"IRINGBUFFER:\n"ANSI_NONE);
+    for(int i = 0; i < RINGBUF_SIZE; i++) printf("%s\n", ringbuf.buffer[ringbuf.index]);
+}
