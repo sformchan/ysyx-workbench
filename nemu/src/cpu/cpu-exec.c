24804fa2da267ed2eae80c4023a785d6ec6d1e90
tracer-ysyx <tracer@ysyx.org>
2025-04-27 00:24:10 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:24:10 up  1:38,  1 user,  load average: 0.26, 0.29, 0.31

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -90,10 +90,10 @@ static void exec_once(Decode *s, vaddr_t pc) {
   char destbuf[128];
   //char instbuf[64];
   //r += snprintf(r, 32, FMT_WORD ":", s->pc);
-  snprintf(destbuf + 1, sizeof(destbuf) / 2, FMT_WORD ":", s->pc);
+  snprintf(destbuf, sizeof(destbuf) / 2, FMT_WORD ":", s->pc);
   //snprintf(instbuf, sizeof(instbuf), " %.8x", s->isa.inst);
   size_t len = strlen(destbuf);
-  snprintf(destbuf + len, sizeof(destbuf) - len, " %.8x", s->isa.inst);
+  snprintf(destbuf + len + 1, sizeof(destbuf) - len, " %.8x", s->isa.inst);
   ringbuf_push(destbuf);
 
   if(nemu_state.state == NEMU_END)
