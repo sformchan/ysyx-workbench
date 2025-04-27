a6af2a91b8d454aa886654edb8fec8966353540a
tracer-ysyx <tracer@ysyx.org>
2025-04-27 23:43:40 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:43:40 up 21 min,  1 user,  load average: 0.41, 0.41, 0.29

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -21,7 +21,6 @@
 bool check_wp();
 void ringbuf_push(const char *log);
 void ringbuf_print();
-void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
@@ -82,7 +81,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
   p += space_len;
-  //void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
 #endif
@@ -103,8 +102,11 @@ static void exec_once(Decode *s, vaddr_t pc) {
     r += snprintf(r, 4, "%02x ", inst_r[j]);
   }
   r += snprintf(r, 8, "  ");
+
+#ifdef CONFIG_ITRACE
   disassemble(r, destbuf + sizeof(destbuf) - r,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, rlen);
+#endif
 
   char *dest = strdup(destbuf);
   //printf("%s\n", destbuf);
