b029249fb25861a182f1311d79ab0c5440bb1eaa
tracer-ysyx <tracer@ysyx.org>
2025-04-28 00:41:19 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:41:19 up  1:19,  1 user,  load average: 0.21, 0.26, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -57,7 +57,7 @@ int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   int len = vsprintf(out, fmt, ap);
-  va_end(ap);
+  //va_end(ap);
   return len;
   //panic("Not implemented");
 }
