f69fcabc2c99a0c66684b8dcb8f2193afd64958c
tracer-ysyx <tracer@ysyx.org>
2025-06-03 22:59:29 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:59:29 up 16 min,  1 user,  load average: 0.14, 0.29, 0.26

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -54,6 +54,11 @@ word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
+
+  #ifdef CONFIG_MTRACE
+  printf("[MTARCE] READ addr = " FMT_PADDR "  len = %d\n", addr, len);
+  #endif
+
   return 0;
 }
 
@@ -61,4 +66,8 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
+
+  #ifdef CONFIG_MTRACE
+  printf("[MTARCE] WRITE addr = " FMT_PADDR "  len = %d\n", addr, len);
+  #endif
 }
