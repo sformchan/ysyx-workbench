bca03c875cc8aa69690289e3a4bb146292388e43
tracer-ysyx <tracer@ysyx.org>
2025-07-11 09:52:38 +0800

    > compile NEMU ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:52:38 up 51 min,  1 user,  load average: 0.29, 0.26, 0.27

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -161,6 +161,7 @@ const char *find_func(uint32_t addr, uint32_t *start_out) {
   void ftrace_exec(uint32_t pc, uint32_t target, uint32_t rd, uint32_t rs1, int32_t imm) {
     if(rd == 1)
     {
+      call_depth++;
       for (int i = 0; i < call_depth; i++)
       {
         printf(" ");
