30cb2816d032a0aec4c4cd59655a5cef04470883
tracer-ysyx <tracer@ysyx.org>
2025-07-25 19:08:21 +0800

    > sim RTL ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:08:21 up  1:07,  1 user,  load average: 0.61, 0.59, 0.42

--- tpc/csrc/sim_main.cpp
+++ tpc/csrc/sim_main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char** argv)
 	inst = pmem_read(top->pc);
 	if(stop)
 	{
-		printf("|0x%08X  |  0x%08X  |  0x%08X  |  0x%08X  |  0x%08X  |\n", top->pc, inst, top->gpr0, top->gpr1, top->gpr2);
+		//printf("|0x%08X  |  0x%08X  |  0x%08X  |  0x%08X  |  0x%08X  |\n", top->pc, inst, top->gpr0, top->gpr1, top->gpr2);
 		printf("\033[33mSimulation stopped successfully due to ebreak!\033[0m\n");
 	}
 	delete top;
