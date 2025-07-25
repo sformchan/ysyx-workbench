16c40c9dbf1fab5364ecc2cfb9510a18bf35f521
tracer-ysyx <tracer@ysyx.org>
2025-07-25 13:52:58 +0800

    > sim RTL ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:52:58 up  1:32,  1 user,  load average: 0.42, 0.33, 0.23

--- tpc/csrc/memory.c
+++ tpc/csrc/memory.c
@@ -47,7 +47,7 @@ extern "C" void pmem_write(int waddr, int wdata, int wmask)
         return;
     }
     //wmask & 0x1 means to check if the lowest bit of wmask is set, if not, DO NOT write.
-    if (wmask & 0x1) rom[waddr + 0] = (wdata >> 0) & 0xFF, printf("byte0 written"); 
+    if (wmask & 0x1) rom[waddr + 0] = (wdata >> 0) & 0xFF, printf("byte0 written\n"); 
     if (wmask & 0x2) rom[waddr + 1] = (wdata >> 8) & 0xFF;
     if (wmask & 0x4) rom[waddr + 2] = (wdata >> 16) & 0xFF;
     if (wmask & 0x8) rom[waddr + 3] = (wdata >> 24) & 0xFF;
