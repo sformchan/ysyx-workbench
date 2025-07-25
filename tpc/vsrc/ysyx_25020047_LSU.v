42de7d739f398b404aad9f181af620125be6a1a7
tracer-ysyx <tracer@ysyx.org>
2025-07-25 12:50:11 +0800

    > sim RTL ysyx_22040000 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:50:11 up 30 min,  1 user,  load average: 0.26, 0.36, 0.36

--- tpc/vsrc/ysyx_25020047_LSU.v
+++ tpc/vsrc/ysyx_25020047_LSU.v
@@ -20,12 +20,13 @@ always @(*) begin
     case(inst_type)
         9'b000100000: memdata = ram_data;
         9'b001000000: begin //lbu
-            case(offset)
-                2'b00: memdata = {24'b0, ram_data[7:0]};
-                2'b01: memdata = {24'b0, ram_data[15:8]};
-                2'b10: memdata = {24'b0, ram_data[23:16]};
-                2'b11: memdata = {24'b0, ram_data[31:24]};
-            endcase
+            memdata = ram_data;
+            // case(offset)
+            //     2'b00: memdata = {24'b0, ram_data[7:0]};
+            //     2'b01: memdata = {24'b0, ram_data[15:8]};
+            //     2'b10: memdata = {24'b0, ram_data[23:16]};
+            //     2'b11: memdata = {24'b0, ram_data[31:24]};
+            // endcase
         end
         default     : memdata = 32'b0;
     endcase
