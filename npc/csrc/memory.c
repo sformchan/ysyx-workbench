60809396d0e80a1795c5120be4e348413b074bdf
tracer-ysyx <tracer@ysyx.org>
2025-07-26 11:34:21 +0800

    > sim RTL ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:34:21 up  2:21,  3 users,  load average: 0.35, 0.41, 0.33

--- npc/csrc/memory.c
+++ npc/csrc/memory.c
@@ -34,31 +34,33 @@ uint8_t rom[ysyx_25020047_ROM_SIZE];
 extern "C" int pmem_read(int raddr)
 {
     raddr &= ~(0x3u);
-    if(raddr + 3 > ysyx_25020047_ROM_SIZE)
+    uint32_t offset = raddr - ysyx_25020047_INITADDR;
+    if(offset + 3 >= ysyx_25020047_ROM_SIZE)
     {
-       // printf("\033[31mError: read_address 0x%08x is out of MEM range.\033[0m\n", raddr);
-        return 0xFFFFFFFF; // 返回错误码
+        printf("\033[31mError: read_address 0x%08x is out of MEM range.\033[0m\n", raddr);
+        return 0xFFFFFFFF;
     }
-    return ((uint32_t)rom[raddr]) |
-           ((uint32_t)rom[raddr + 1] << 8) |
-           ((uint32_t)rom[raddr + 2] << 16) |
-           ((uint32_t)rom[raddr + 3] << 24);
+    return ((uint32_t)rom[offset]) |
+           ((uint32_t)rom[offset + 1] << 8) |
+           ((uint32_t)rom[offset + 2] << 16) |
+           ((uint32_t)rom[offset + 3] << 24);
 }
+
 
 extern "C" void pmem_write(int waddr, int wdata, int wmask)
 {
-    int byte_offset = waddr & 0x3;
     waddr &= ~(0x3u);
-    if(waddr + 3 > ysyx_25020047_ROM_SIZE)
+    uint32_t offset = waddr - ysyx_25020047_INITADDR;
+    if(offset + 3 >= ysyx_25020047_ROM_SIZE)
     {
-        //printf("\033[31mError: write_address 0x%08x is out of MEM range.\033[0m\n", waddr);
+        printf("\033[31mError: write_address 0x%08x is out of MEM range.\033[0m\n", waddr);
         return;
     }
-    //wmask & 0x1 means to check if the lowest bit of wmask is set, if not, DO NOT write.
-    if (wmask & 0x1) rom[waddr + 0] = (wdata >> 0) & 0xFF /*, printf("byte0 written to 0x%08x: 0x%02x\n", waddr,rom[waddr + 0])*/; 
-    if (wmask & 0x2) rom[waddr + 1] = (wdata >> 8) & 0xFF /*, printf("byte1 written to 0x%08x: 0x%02x\n", waddr + 1, rom[waddr + 1])*/; 
-    if (wmask & 0x4) rom[waddr + 2] = (wdata >> 16) & 0xFF /*, printf("byte2 written to 0x%08x: 0x%02x\n", waddr + 2, rom[waddr + 2])*/; 
-    if (wmask & 0x8) rom[waddr + 3] = (wdata >> 24) & 0xFF /*, printf("byte3 written to 0x%08x: 0x%02x\n", waddr + 3, rom[waddr + 3])*/; 
+	//wmask & 0x1 means to check if the lowest bit of wmask is set, if not, DO NOT write.
+    if (wmask & 0x1) rom[offset + 0] = (wdata >> 0) & 0xFF;
+    if (wmask & 0x2) rom[offset + 1] = (wdata >> 8) & 0xFF;
+    if (wmask & 0x4) rom[offset + 2] = (wdata >> 16) & 0xFF;
+    if (wmask & 0x8) rom[offset + 3] = (wdata >> 24) & 0xFF;
 }
 
 
