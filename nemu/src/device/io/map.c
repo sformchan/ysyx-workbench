f0f65d2474c652a9411ef918996c17b7a5336cee
tracer-ysyx <tracer@ysyx.org>
2025-07-27 16:28:39 +0800

    > compile NEMU ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:28:39 up  7:29,  1 user,  load average: 0.21, 0.22, 0.28

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -59,7 +59,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  #ifdef CONFIG_DTRACE
   printf("[DTRACE]" ANSI_FG_CYAN" READ FROM (%s)"ANSI_NONE" : %u\n" , map->name, ret);
+  #endif
   return ret;
 }
 
@@ -67,7 +69,9 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
+  #ifdef CONFIG_DTRACE
   printf("[DTRACE]" ANSI_FG_MAGENTA" WRITE  TO" ANSI_NONE " (%s) : %u\n" , map->name, data);
+  #endif
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
 }
