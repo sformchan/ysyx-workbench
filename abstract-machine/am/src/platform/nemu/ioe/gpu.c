9719d6aa0696e45a185025d85d7e678d6b685938
tracer-ysyx <tracer@ysyx.org>
2025-07-28 12:51:32 +0800

    > compile NEMU ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:51:32 up  3:37,  1 user,  load average: 0.31, 0.33, 0.32

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -15,24 +15,24 @@ void __am_gpu_init() {
 }
 
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-	uint32_t rawdata = inl(RTC_ADDR);
-	uint32_t width = rawdata >> 16;
-	uint32_t height = rawdata & 0xffff;
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = width, .height = height,
-    .vmemsz = width * height * sizeof(uint32_t)
-  };
-}
-
 // void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+// 	uint32_t rawdata = inl(RTC_ADDR);
+// 	uint32_t width = rawdata >> 16;
+// 	uint32_t height = rawdata & 0xffff;
 //   *cfg = (AM_GPU_CONFIG_T) {
 //     .present = true, .has_accel = false,
-//     .width = 400, .height = 300,
-//     .vmemsz = 400 * 300 * sizeof(uint32_t)
+//     .width = width, .height = height,
+//     .vmemsz = width * height * sizeof(uint32_t)
 //   };
 // }
+
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  *cfg = (AM_GPU_CONFIG_T) {
+    .present = true, .has_accel = false,
+    .width = 400, .height = 300,
+    .vmemsz = 400 * 300 * sizeof(uint32_t)
+  };
+}
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	//uint32_t *pixels = (uint32_t *)(uintptr_t)ctl->pixels;
