d4155f01373f95c5e6ae37e21e46fafd7fb5bb23
tracer-ysyx <tracer@ysyx.org>
2025-07-28 11:16:45 +0800

    > sim RTL ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:16:45 up  2:02,  1 user,  load average: 0.38, 0.32, 0.24

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
 	printf("welcome to \033[44;36mNPC\033[0m!\n");
 	
 	//load_sum/mem
-	load_verilog_hex("/home/leonard/Desktop/mem.hex");
+	load_verilog_hex("/home/leonard/Desktop/sum.hex");
 	// parse_args(argc, argv);
 	// load_img();
 	
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
 	//tfp->open("wave.fst");
 	top->rst = 1;
 	top->clk = 0;
-	top->pc = 0x00000000;
+	top->pc = ysyx_25020047_INITADDR;
 	int inst = 0;
 	uint32_t count = 0;
     //printf("|pc          |  inst        |  gpr0        |  gpr1        |  gpr2        |\n");
