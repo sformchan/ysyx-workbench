14f3f08be405c5fb77f3ca237c29e51ebbaef926
tracer-ysyx <tracer@ysyx.org>
2025-07-28 20:53:42 +0800

    > compile NEMU ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:53:42 up 11:39,  1 user,  load average: 0.86, 0.69, 0.60

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,20 +13,6 @@
 // }
 
 // printf implementation using your vsprintf
-int printf(const char *fmt, ...) {
-	char buffer[512];  // fixed buffer size; adjust as needed
-	va_list ap;
-	va_start(ap, fmt);
-	int len = vsprintf(buffer, fmt, ap);
-	va_end(ap);
-  
-	for (int i = 0; i < len; i++) {
-	  putch(buffer[i]);  // output each char
-	}
-  
-	return len;
-}
-
 static int utoa(unsigned int num, char *buf, int base, int uppercase) {
 	const char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
 	int len = 0;
@@ -37,264 +23,113 @@ static int utoa(unsigned int num, char *buf, int base, int uppercase) {
 	return len;
   }
   
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  char *ptr = out;
-  while (*fmt) {
-    if (*fmt == '%') {
-      fmt++;
-
-      // Parse flags '-' and '0'
-      int left_align = 0;
-      int zero_pad = 0;
-      while (*fmt == '-' || *fmt == '0') {
-        if (*fmt == '-') left_align = 1;
-        else if (*fmt == '0') zero_pad = 1;
-        fmt++;
-      }
-
-      // Parse field width
-      int width = 0;
-      while (*fmt >= '0' && *fmt <= '9') {
-        width = width * 10 + (*fmt - '0');
-        fmt++;
-      }
-
-      // Parse precision
-      int precision = -1;  // -1 means no precision specified
-      if (*fmt == '.') {
-        fmt++;
-        precision = 0;
-        while (*fmt >= '0' && *fmt <= '9') {
-          precision = precision * 10 + (*fmt - '0');
-          fmt++;
-        }
-      }
-
-      if (*fmt == '%') {
-        *ptr++ = '%';
-        fmt++;
-        continue;
-      }
-
-      switch (*fmt) {
-        case 'd': {
-          int num = va_arg(ap, int);
-          unsigned int unum;
-          char buf[32];
-          int len = 0;
-
-          if (num < 0) {
-            unum = (unsigned int)(-(num + 1)) + 1;  // safe INT_MIN handling
-          } else {
-            unum = (unsigned int)num;
-          }
-
-          // Convert number to string in reverse order
-          do {
-            buf[len++] = '0' + (unum % 10);
-            unum /= 10;
-          } while (unum > 0);
-
-          // Calculate zeros to pad for precision
-          int zeros_to_pad = 0;
-          if (precision > len) zeros_to_pad = precision - len;
-
-          // Width padding excludes zeros added by precision and sign
-          int width_padding = width - zeros_to_pad - len - (num < 0 ? 1 : 0);
-          if (width_padding < 0) width_padding = 0;
-
-          // According to standard, if precision specified, zero_pad flag ignored
-          if (precision >= 0) zero_pad = 0;
-
-          if (!left_align) {
-            char pad_char = zero_pad ? '0' : ' ';
-            for (int i = 0; i < width_padding; i++) *ptr++ = pad_char;
-          }
-
-          // Print sign if negative
-          if (num < 0) *ptr++ = '-';
-
-          // Print zeros for precision
-          for (int i = 0; i < zeros_to_pad; i++) *ptr++ = '0';
-
-          // Print digits
-          while (len--) *ptr++ = buf[len];
-
-          if (left_align) {
-            for (int i = 0; i < width_padding; i++) *ptr++ = ' ';
-          }
-
-          fmt++;
-          break;
-        }
+  int vsprintf(char *out, const char *fmt, va_list ap) {
+	char *ptr = out;
+	while (*fmt) {
+	  if (*fmt != '%') {
+		*ptr++ = *fmt++;
+		continue;
+	  }
+  
+	  fmt++;  // skip '%'
+	  int left_align = 0, zero_pad = 0, width = 0;
+	  if (*fmt == '-') { left_align = 1; fmt++; }
+	  if (*fmt == '0') { zero_pad = 1; fmt++; }
+  
+	  while (*fmt >= '0' && *fmt <= '9') {
+		width = width * 10 + (*fmt++ - '0');
+	  }
+  
+	  if (*fmt == '%') {
+		*ptr++ = '%'; fmt++; continue;
+	  }
+  
+	  char buf[32];
+	  int len = 0;
+  
+	  switch (*fmt) {
+		case 'd': {
+		  int num = va_arg(ap, int);
+		  unsigned int unum = (num < 0) ? -num : num;
+		  len = utoa(unum, buf, 10, 0);
+		  if (num < 0) buf[len++] = '-';
+		  break;
+		}
 		case 'u': {
-			unsigned int num = va_arg(ap, unsigned int);
-			char buf[32];
-			int len = utoa(num, buf, 10, 0);
-		  
-			int zeros_to_pad = (precision > len) ? precision - len : 0;
-			int width_padding = width - zeros_to_pad - len;
-			if (width_padding < 0) width_padding = 0;
-		  
-			if (precision >= 0) zero_pad = 0;
-		  
-			if (!left_align) {
-			  char pad_char = zero_pad ? '0' : ' ';
-			  for (int i = 0; i < width_padding; i++) *ptr++ = pad_char;
-			}
-		  
-			for (int i = 0; i < zeros_to_pad; i++) *ptr++ = '0';
-		  
-			while (len--) *ptr++ = buf[len];
-		  
-			if (left_align) {
-			  for (int i = 0; i < width_padding; i++) *ptr++ = ' ';
-			}
-		  
-			fmt++;
-			break;
-		  }
-		  case 'x':
-		  case 'X': {
-			unsigned int num = va_arg(ap, unsigned int);
-			char buf[32];
-			int uppercase = (*fmt == 'X');
-			int len = utoa(num, buf, 16, uppercase);
-		  
-			int zeros_to_pad = (precision > len) ? precision - len : 0;
-			int width_padding = width - zeros_to_pad - len;
-			if (width_padding < 0) width_padding = 0;
-		  
-			if (precision >= 0) zero_pad = 0;
-		  
-			if (!left_align) {
-			  char pad_char = zero_pad ? '0' : ' ';
-			  for (int i = 0; i < width_padding; i++) *ptr++ = pad_char;
-			}
-		  
-			for (int i = 0; i < zeros_to_pad; i++) *ptr++ = '0';
-		  
-			while (len--) *ptr++ = buf[len];
-		  
-			if (left_align) {
-			  for (int i = 0; i < width_padding; i++) *ptr++ = ' ';
-			}
-		  
-			fmt++;
-			break;
-		  }
-		  case 'o': {
-			unsigned int num = va_arg(ap, unsigned int);
-			char buf[32];
-			int len = utoa(num,  buf, 8, 0);
-		  
-			int zeros_to_pad = (precision > len) ? precision - len : 0;
-			int width_padding = width - zeros_to_pad - len;
-			if (width_padding < 0) width_padding = 0;
-		  
-			if (precision >= 0) zero_pad = 0;
-		  
-			if (!left_align) {
-			  char pad_char = zero_pad ? '0' : ' ';
-			  for (int i = 0; i < width_padding; i++) *ptr++ = pad_char;
-			}
-		  
-			for (int i = 0; i < zeros_to_pad; i++) *ptr++ = '0';
-		  
-			while (len--) *ptr++ = buf[len];
-		  
-			if (left_align) {
-			  for (int i = 0; i < width_padding; i++) *ptr++ = ' ';
-			}
-		  
-			fmt++;
-			break;
-		  }
-		  case 'p': {
-			uintptr_t ptrval = (uintptr_t)va_arg(ap, void *);
-			char buf[32];
-			int len = utoa(ptrval, buf, 16, 0);
-		  
-			// "0x" prefix
-			*ptr++ = '0';
-			*ptr++ = 'x';
-		  
-			while (len--) *ptr++ = buf[len];
-		  
-			fmt++;
-			break;
-		  }
-				
-        case 's': {
-          const char *str = va_arg(ap, const char *);
-          int len = 0;
-          while (str[len] != '\0') len++;
-
-          // Precision limits max chars printed for %s
-          if (precision >= 0 && precision < len) {
-            len = precision;
-          }
-
-          int width_padding = width - len;
-          if (width_padding < 0) width_padding = 0;
-
-          if (!left_align) {
-            for (int i = 0; i < width_padding; i++) *ptr++ = ' ';
-          }
-
-          for (int i = 0; i < len; i++) {
-            *ptr++ = str[i];
-          }
-
-          if (left_align) {
-            for (int i = 0; i < width_padding; i++) *ptr++ = ' ';
-          }
-
-          fmt++;
-          break;
-        }
+		  unsigned int num = va_arg(ap, unsigned int);
+		  len = utoa(num, buf, 10, 0);
+		  break;
+		}
+		case 'x': case 'X': {
+		  unsigned int num = va_arg(ap, unsigned int);
+		  len = utoa(num, buf, 16, *fmt == 'X');
+		  break;
+		}
+		case 'o': {
+		  unsigned int num = va_arg(ap, unsigned int);
+		  len = utoa(num, buf, 8, 0);
+		  break;
+		}
 		case 'c': {
-			char ch = (char)va_arg(ap, int);  // promoted to int in variadic args
-			int width_padding = width > 1 ? width - 1 : 0;
-		  
-			if (!left_align) {
-			  for (int i = 0; i < width_padding; i++) *ptr++ = ' ';
-			}
-		  
-			*ptr++ = ch;
-		  
-			if (left_align) {
-			  for (int i = 0; i < width_padding; i++) *ptr++ = ' ';
-			}
-		  
-			fmt++;
-			break;
-		  }
-		  
-        default: {
-          // Unsupported specifier: print literally
-          *ptr++ = '%';
-          if (*fmt) {
-            *ptr++ = *fmt++;
-          }
-          break;
-        }
-      }
-    } else {
-      *ptr++ = *fmt++;
-    }
+		  char ch = (char)va_arg(ap, int);
+		  *ptr++ = ch;
+		  fmt++;
+		  continue;
+		}
+		case 's': {
+		  const char *str = va_arg(ap, const char *);
+		  while (*str) *ptr++ = *str++;
+		  fmt++;
+		  continue;
+		}
+		case 'p': {
+		  uintptr_t p = (uintptr_t)va_arg(ap, void *);
+		  *ptr++ = '0'; *ptr++ = 'x';
+		  len = utoa((unsigned int)(p), buf, 16, 0);  // assume 32-bit pointer
+		  break;
+		}
+		default: {
+		  *ptr++ = '%';
+		  if (*fmt) *ptr++ = *fmt;
+		  fmt++;
+		  continue;
+		}
+	  }
+  
+	  // padding
+	  int padding = width - len;
+	  if (!left_align) {
+		char pad = zero_pad ? '0' : ' ';
+		while (padding-- > 0) *ptr++ = pad;
+	  }
+	  while (len-- > 0) *ptr++ = buf[len];
+	  if (left_align) while (padding-- > 0) *ptr++ = ' ';
+	  fmt++;
+	}
+  
+	*ptr = '\0';
+	return ptr - out;
   }
-  *ptr = '\0';
-  return ptr - out;
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int len = vsprintf(out, fmt, ap);
-  va_end(ap);
-  return len;
-}
+  
+  int sprintf(char *out, const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	int len = vsprintf(out, fmt, ap);
+	va_end(ap);
+	return len;
+  }
+  
+  int printf(const char *fmt, ...) {
+	char buffer[512];
+	va_list ap;
+	va_start(ap, fmt);
+	int len = vsprintf(buffer, fmt, ap);
+	va_end(ap);
+	for (int i = 0; i < len; i++) {
+	  putch(buffer[i]);
+	}
+	return len;
+  }
 
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
