92cfe7a797b4543c3f3ce908ca66794d8c6c3f2a
tracer-ysyx <tracer@ysyx.org>
2025-07-31 14:00:54 +0800

    > sim RTL ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:00:54 up  4:52,  1 user,  load average: 0.49, 0.28, 0.15

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -33,5 +33,5 @@ bool ioe_init() {
   return true;
 }
 
-void ioe_read (int reg, void *buf) { /*printf("%d\n", reg);*/ ((handler_t)lut[reg])(buf); }
-void ioe_write(int reg, void *buf) { printf("%d\n", reg); ((handler_t)lut[reg])(buf); }
+void ioe_read (int reg, void *buf) { printf("%d\n", reg); ((handler_t)lut[reg])(buf); }
+void ioe_write(int reg, void *buf) { /*printf("%d\n", reg);*/ ((handler_t)lut[reg])(buf); }
