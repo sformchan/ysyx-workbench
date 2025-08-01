3501ba56d419584e9d908f9b7ed17b7d7a8b83ce
tracer-ysyx <tracer@ysyx.org>
2025-08-01 20:36:43 +0800

    > sim RTL ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:36:43 up  5:14,  1 user,  load average: 0.21, 0.26, 0.25

--- npc/csrc/state.c
+++ npc/csrc/state.c
@@ -17,7 +17,7 @@ extern "C" void execute()
 		// {
 		// 	printf("|0x%08X  |  0x%08X  |  0x%08X  |  0x%08X  |  0x%08X  |\n", top->pc, inst, top->gpr0, top->gpr1, top->gpr2);
 		// }
-		if(!top->clk)
+		if(top->clk)
 		{
 			count++;
 			printf("|0x%08X  |  0x%08X  |  %08d   |\n", top->pc, inst, count);
