6a67e93f8b7e9527f2f53a8652420dccaf657b22
tracer-ysyx <tracer@ysyx.org>
2025-08-02 11:57:43 +0800

    > sim RTL ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:57:43 up  1:33,  1 user,  load average: 0.15, 0.17, 0.17

--- npc/config.mk
+++ npc/config.mk
@@ -1,16 +1,22 @@
 KCONFIG_CONFIG := .config
 
 menuconfig:
-	scripts/kconfig/build/mconf Kconfig
+	tools/kconfig/build/mconf Kconfig
+	tools/kconfig/build/conf --syncconfig Kconfig
 
 autoconf.h: .config
 	@mkdir -p include/config
-	@cat .config | sed 's/^CONFIG_/#define CONFIG_/g' > include/config/autoconf.h
+	@grep -E '^CONFIG_' .config | \
+	sed -E 's/^(CONFIG_[^=]+)=y/#define \1 1/' | \
+	sed -E 's/^(CONFIG_[^=]+)=n|# CONFIG_[^ ]+ is not set/\/\* \1 is not set \*\//' | \
+	sed -E 's/^(CONFIG_[^=]+)=(.+)/#define \1 \2/' \
+	> include/config/autoconf.h
 
 defconfig:
-	scripts/kconfig/build/conf --defconfig=defconfig Kconfig
+	tools/kconfig/build/conf --defconfig=defconfig Kconfig
 
 clean:
 	rm -f .config include/config/autoconf.h
 
 .PHONY: menuconfig defconfig clean
+
