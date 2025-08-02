1afafe96538eaba4755515fe8b010458efea080f
tracer-ysyx <tracer@ysyx.org>
2025-08-02 13:54:27 +0800

    > compile NEMU ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:54:27 up  3:30,  1 user,  load average: 0.24, 0.34, 0.27

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -101,7 +101,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #endif
     r += snprintf(r, 4, "%02x ", inst_r[j]);
   }
-  r += snprintf(r, 8, "  ");
+  //r += snprintf(r, 8, "  ");
+  r += snprintf(r, 8, "%*s", space_len, " ");
 
 #ifdef CONFIG_ITRACE
   disassemble(r, destbuf + sizeof(destbuf) / 2 + 20 - r,
