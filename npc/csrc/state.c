d28375254e874886be4fe4b96f6b2968c8c4b57f
tracer-ysyx <tracer@ysyx.org>
2025-08-05 20:52:29 +0800

    > sim RTL ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:52:29 up 11:03,  1 user,  load average: 0.61, 0.40, 0.32

--- npc/csrc/state.c
+++ npc/csrc/state.c
@@ -148,7 +148,7 @@ extern "C" void run_npc(uint64_t step)
 			printf("\033[44;36mNPC\033[0m" ANSI_FG_GREEN " HIT GOOD TRAP " ANSI_NONE "at pc 0x%08x (%d cycle(s))\n" , top->pc, count);
 			break;
 		case NPC_ABORT:
-			printf(ANSI_FG_WHITE "npc_state " ANSI_NONE "= " ANSI_FG_RED "NPC_ABORT.\n" ANSI_NONE);
+			//printf(ANSI_FG_WHITE "npc_state " ANSI_NONE "= " ANSI_FG_RED "NPC_ABORT.\n" ANSI_NONE);
 			printf("\033[44;36mNPC\033[0m" ANSI_FG_RED " ABORT " ANSI_NONE "at pc 0x%08x (%d cycle(s))\n", top->pc, count);
 			break;
 		
