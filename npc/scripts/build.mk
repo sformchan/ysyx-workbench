432401ec0e82e89cb96aa7de2cfc791a92514f42
tracer-ysyx <tracer@ysyx.org>
2025-08-05 18:20:31 +0800

    > sim RTL ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:20:31 up  8:31,  1 user,  load average: 0.03, 0.14, 0.11

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -52,7 +52,7 @@ $(BIN): $(VSRCS) $(CSRCS) $(NPC_HOME)/include/generated/autoconf.h
 all: default
 		
 include $(NPC_HOME)/scripts/difftest.mk
-$(nemuhome = $(DIFF_REF_PATH))
+$(info nemuhome = $(DIFF_REF_PATH))
 IMG ?=
 override ARGS ?= --log=$(BUILD_DIR)/npc-log.txt
 override ARGS += $(ARGS_DIFF)
