97f969394623698ded7d8b9ce070f219ddb7e54b
tracer-ysyx <tracer@ysyx.org>
2025-08-08 20:28:14 +0800

    > compile NEMU ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:28:14 up 10:58,  1 user,  load average: 0.14, 0.29, 0.32

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -71,9 +71,9 @@ void sim_t::diff_set_regs(void* diff_context) {
     state->XPR.write(i, (sword_t)ctx->gpr[i]);
   }
   state->pc = ctx->pc;
-  state->mepc->val = ctx->mepc;
-  state->mcause->val = ctx->mcause;
-  state->mstatus->val = ctx->mstatus;
+  state->mepc->write(ctx->mepc);
+  state->mcause->write(ctx->mcause);
+  state->mstatus->write(ctx->mstatus);
 }
 
 void sim_t::diff_memcpy(reg_t dest, void* src, size_t n) {
