7d005668722feece912bccbb7a80b6882d11dbd2
tracer-ysyx <tracer@ysyx.org>
2025-08-10 14:40:44 +0800

    > sim RTL ysyx_25020047 陈树锋 Linux leonard-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:44 up  5:44,  3 users,  load average: 0.63, 0.52, 0.46

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -4,20 +4,35 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
+// Context* __am_irq_handle(Context *c) {
+//   if (user_handler) {
+//     Event ev = {0};
+//     switch (c->mcause) {
+// 		case 11: ev.event = EVENT_YIELD; c->mepc += 4; break; 
+//       default: ev.event = EVENT_ERROR; break;
+//     }
+
+//     c = user_handler(ev, c);
+//     assert(c != NULL);
+//   }
+
+//   return c;
+// }
 Context* __am_irq_handle(Context *c) {
-  if (user_handler) {
-    Event ev = {0};
-    switch (c->mcause) {
-		case 11: ev.event = EVENT_YIELD; c->mepc += 4; break; 
-      default: ev.event = EVENT_ERROR; break;
-    }
-
-    c = user_handler(ev, c);
-    assert(c != NULL);
+	if (user_handler) {
+	  Event ev = {0};
+	  switch (c->mcause) {
+		  case 11: ev.event = EVENT_YIELD; c->mepc += 4; break;
+		default: ev.event = EVENT_ERROR; break;
+	  }
+  
+	  c = user_handler(ev, c);
+	  assert(c != NULL);
+	}
+	//printf("Switch to ctx at %p, mepc = 0x%08x\n", c, c->mepc);
+  
+	return c;
   }
-
-  return c;
-}
 
 extern void __am_asm_trap(void);
 
